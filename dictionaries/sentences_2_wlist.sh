cfc8f1f909c8f783dcbf4dbe07f0f7abeb0be32a
jluis <jluisg.23@gmail.com>
2018-05-16 23:37:38 -0500

    Removal of deprecated files

--- dictionaries/sentences_2_wlist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tr -d '.?!,:;' | tr [:space:] '\n' | tr [:lower:] [:upper:] | sort | uniq > /tmp/wlist.tmp
-N_words=$(wc -l < "/tmp/wlist.tmp")
-((N_words--))
-
-echo "N_words = $N_words"
-tail -n${N_words} "/tmp/wlist.tmp" > "wlist"
